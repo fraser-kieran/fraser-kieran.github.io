2447ff4d38ccf814431456d7997392cbbd17d3fb
Kieran Fraser <kfraser@tcd.ie>
2020-05-15 13:51:29 +0100

    Updated firebase libraries

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,10 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-auth.js');
-importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-database.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-auth.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js');
 
 var firebaseConfig = {
     apiKey: "AIzaSyAQu435FNTmXIcFigctYMdRO8sPLX78HI0",

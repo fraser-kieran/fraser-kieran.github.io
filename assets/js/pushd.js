51f1468e446f42910ebe652e44c7ba1f9b26f455
Kieran Fraser <kfraser@tcd.ie>
2020-07-22 15:53:27 +0100

    Updated all

--- assets/js/pushd.js
+++ assets/js/pushd.js
@@ -1,41 +1,10 @@
-var notificationString = '<div class="card" style="width: 100%; margin-bottom: 10px">'+
-      '<div class="card-body" style="padding: 10px;">'+
-        '<div class="container-fluid" style="width: 100%; margin: 0px; padding: 0px;">'+
-            '<div class="row">'+
-                '<div class="col-md-2">'+
-                    '<img src="https://{{:domain}}/favicon.ico" alt=" " style="height: 35px; padding: 5px; border-radius: 50%; border-radius: 50%;border: solid;border-width: thin;    border-color: black;">'+
-                '</div>'+
-                '<div class="col-md-10">'+
-                    '<h5 class="card-title" style="padding: 10px;">{{:title}}</h5>'+
-                '</div>'  +
-            '</div>'+
-        '</div>'  +
-        /*'<p class="card-text">{{:ticker}}</p>'+*/
-        '<div style="text-align: center; margin: 0px; padding: 0px;">'+
-            '<div class="btn-group btn-group-toggle" data-toggle="buttons">'+
-              '<label class="btn btn-secondary" name="open" id="{{:id}}">'+
-                '<input type="radio" autocomplete="off">Open'+
-              '</label>'+
-              '<label class="btn btn-secondary" name="dismiss" id="{{:id}}">'+
-                '<input type="radio" autocomplete="off">Dismiss'+
-              '</label>'+
-            '</div>'+
-        '</div>'+
-        '<p style="text-align: right; font-size: small; margin: 0px;">{{:domain}}</p>'+
-      '</div>'+
-    '</div>'
-var notificationTemplate = $.templates(notificationString);
-
-var notificationList = []
-var mindfulScores = []
-var curiosityScores = []
 var prolificURL = ''
+var pushAttempts = 0
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
-        getAllNotifications()
         getProlificURLS()
     }
 });
@@ -52,58 +21,6 @@ function contactFormInit(){
         }
     }
     catch(e){}
-}
-
-function mindfulModal(){
-    $('#mindfulModal').modal('show')
-} 
-
-function closeMindfulModal(){
-    $('#mindfulModal').modal('hide')
-}
-
-function saveMindfulModal(){
-    mindfulScores = []
-    $('#mindfulError').hide()
-    var scores = $('#mindfulModal .btn-likert').map(function() { 
-        if(!this.classList.contains("disabled") && this.classList.contains("active")) 
-            return this.innerText
-    }).get();
-    if(scores.length == 15){
-        mindfulScores = scores
-        $('#mindfulModal').modal('hide')
-        $('#mindfulError').hide()
-        $('#mindfulScaleButton').removeClass('btn-primary').addClass('btn-success')
-    }
-    else{
-        $('#mindfulError').show()
-    }
-}
-
-function curiosityModal(){
-    $('#curiosityModal').modal('show')
-}
-
-function closeCuriosityModal(){
-    $('#curiosityModal').modal('hide')
-}
-
-function saveCuriosityModal(){
-    curiosityScores = []
-    $('#curiosityError').hide()
-    var scores = $('#curiosityModal .btn-likert').map(function() { 
-        if(!this.classList.contains("disabled") && this.classList.contains("active")) 
-            return this.innerText
-    }).get();
-    if(scores.length == 10){
-        curiosityScores = scores
-        $('#curiosityModal').modal('hide')
-        $('#curiosityError').hide()
-        $('#curiosityInventoryButton').removeClass('btn-primary').addClass('btn-success')
-    }
-    else{
-        $('#curiosityError').show()
-    }
 }
 
 function checkChrome(){
@@ -196,8 +113,38 @@ function checkAndroid() {
     }
 } 
 
-function signup(){
-    checkAndroid()
+function prescreenNoti(){
+    if (Notification.permission === 'denied') {
+        $('#subButton').prop('disabled', true);
+
+        $.confirm({
+            title: "<h2 style='text-transform: capitalize; letter-spacing: normal; line-spacing: normal;'>Notification Permissions</h2>",
+            content: "<h5 style='text-transform: none; letter-spacing: normal; line-spacing: normal;'>To receive <strong style='text-transform: capitalize;'>Pushd</strong> notifications from the content creators on this platform, you must grant push-notification permission to <strong style='text-transform: capitalize;'>Pushd</strong>.</h5>",
+            type: 'blue',
+            useBootstrap: true,
+            typeAnimated: true,
+            buttons: {
+                grant: {
+                    text: 'Grant',
+                    btnClass: 'btn-white',
+                    action: function(){
+                        askPermission()
+                    }
+                },
+                deny: {
+                    text: 'Deny',
+                    btnClass: 'btn-white',
+                    action: function(){
+                        return;
+                    }
+                }
+            },
+            draggable: false,
+        });
+    }
+    else{
+        checkAndroid()
+    }
 }
 
 function savePrefs(){
@@ -210,55 +157,16 @@ function savePrefs(){
     }catch(e){}
 
 
-    //prolificId = $('#prolificId').val()
-    gender = $('#gender').val()
-    ages = $('#ages').val()
-    topics = $('#topicPrefs').val()
-    posWeb = $('#posWebPrefs').val()
-    negWeb = $('#negWebPrefs').val()
-
-    opened = []
-    dismissed = []
-
-    $("#notificationFeed .active").each(function() {
-        var name = $(this).attr('name');
-        var id = $(this).attr('id');
-        if(name=='open')
-            opened.push(id)
-        else
-            dismissed.push(id)
-   });
-
-    if(gender==null || ages==null){
-        $.alert({
-            title: 'Wait!',
-            type: 'orange',
-            content: 'You must select a gender <strong>and</strong> age group...',
-        });
-        return
-    }
-
-    if(topics.length<2|posWeb.length<2|negWeb.length<2|(opened.length+dismissed.length)<5){
-        $.alert({
-            title: 'Wait!',
-            type: 'orange',
-            content: 'You must select at least 2 topics<br><strong>and</strong> at least 4 websites (2 positive and 2 negative)<br><strong>and</strong> open or dismiss at least 5 generated notifications...',
-        });
-        return
-    }
-
-    if(mindfulScores<15 || curiosityScores<10){
-        $.alert({
-            title: 'Wait!',
-            type: 'orange',
-            content: 'You\'re almost finished! Please complete the Mindful Scale and Curiosity Inventory...',
-        });
-        return
-    }
-
-    savePreferences(prolificId, gender, ages, topics, posWeb, negWeb, opened, dismissed)
-
     if(prolificId && prolificId!=''){
+        
+        userId = document.cookie.split('=')[1]
+        if(userId.charAt(0) == '-'){
+            database.ref('participant/'+userId).update({
+                prolificId: prolificId,
+                prescreenComplete: Math.round((new Date()).getTime())
+            });
+        }
+        
         $.confirm({
             title: 'Finished',
             type: 'green',
@@ -271,91 +179,10 @@ function savePrefs(){
             }
         });
     } 
-    else{
-        $.confirm({
-            title: 'Finished',
-            type: 'green',
-            content: 'You are now set up. Simply engage with incoming notifications as normal over the next three weeks. You will receive a short (max 30 seconds) questionnaire every night via notification which we ask you to complete. You will receive a notification in three weeks indicating you have finished the study. Thanks for participating!',
-            buttons: {
-                confirm: function () {
-                    $('#exampleModal').modal('hide')
-                }
-            }
-        });
-    }
-
-}
-
-function savePreferences(prolificId, gender, ages, topics, posWeb, negWeb, opened, dismissed) {
-
-
-    userId = document.cookie.split('=')[1]
-    if(userId.charAt(0) == '-'){
-        database.ref('participant/'+userId).update({
-            gender: gender,
-            ages: ages,
-            topics: topics,
-            pos_web: posWeb,
-            neg_web: negWeb,
-            mindful_scores: mindfulScores,
-            curiosity_scores: curiosityScores,
-            init_open: opened,
-            init_dismissed: dismissed,
-            timezone: (new Date()).getTimezoneOffset(),
-            prolificId: prolificId
-        });
-    }
-}
-
-function generateTestNotifications(){
-    posWeb = $('#posWebPrefs').val()
-    negWeb = $('#negWebPrefs').val()
-
-    notifications = []
-    websites = posWeb.concat(negWeb)
-    for(notification of notificationList){
-        if(websites.some(website => notification.domain.includes(website)))
-            notifications.push({title: notification.summary, ticker: notification.summary,
-                              id: notification.id, domain: notification.domain})
-    }
-
-    var html = notificationTemplate.render(getRandom(notifications, notifications.length<=10?notifications.length : 10));
-    $( "#notificationFeed" ).empty()
-    $( "#feedInstructions" ).show()
-    $( "#notificationFeed" ).append(html)
-
-}
-
-function getRandom(arr, n) {
-    var result = new Array(n),
-        len = arr.length,
-        taken = new Array(len);
-    if (n > len)
-        throw new RangeError("getRandom: more elements taken than available");
-    while (n--) {
-        var x = Math.floor(Math.random() * len);
-        result[n] = arr[x in taken ? taken[x] : x];
-        taken[x] = --len in taken ? taken[len] : len;
-    }
-    return result;
-}
-
-function getAllNotifications(){
-    var pushRef = database.ref('web_notifications/')
-    .once('value')
-    .then(function(snapshot) {
-
-        notificationList = []
-        if(snapshot.val()!=null){
-            snapshot.forEach((notification)=> {
-                notificationList.push(notification.val())
-            });
-        }
-    });
 }
 
 function getProlificURLS(){
-    var pushRef = database.ref('prolific_urls/start')
+    var pushRef = database.ref('prolific_urls/prescreen')
     .once('value')
     .then(function(snapshot) {
         if(snapshot && snapshot.val()!=null){
@@ -364,6 +191,36 @@ function getProlificURLS(){
     });
 }
 
-function cancelParticipation(){
-    subButtonClick()
+function sendPrescreenNotification(){
+    if(pushAttempts<3){
+        
+        userId = document.cookie.split('=')[1]
+        if(userId.charAt(0) == '-' && prolificURL!=''){
+            var prescreenPushURL = "https://empushy.azurewebsites.net/v1/pushd/prescreen-push";
+
+            var formData = JSON.stringify({
+                "userId": userId,
+                "url": prolificURL
+            })
+
+            fetch(prescreenPushURL, {
+                method: 'post',
+                headers: {
+                  "Content-type": "application/json; charset=utf-8"
+                },
+                body: formData
+            })
+        }
+        pushAttempts += 1 
+        $('#pushNotiButton').attr('disabled', true);
+        
+        setTimeout(function() { 
+            $('#pushNotiButton').attr('disabled', true);
+        }, 5000);
+        
+    }  
+    else{
+        if(prolificURL!='')
+            window.location.replace(prolificURL);
+    }
 }
